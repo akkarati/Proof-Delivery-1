1225e143a3cc81d3bd07ebb1e52f20925d771982
Vipavee <vi_vpk_4ever@hotmail.com>
2016-10-11 13:50:11 +0700

    Last

--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/MyAlert.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/MyAlert.java
@@ -1,24 +1,25 @@
 package com.hitachi_tstv.yodpanom.yaowaluk.proofdelivery;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 
 /**
- * Created by Vipavee on 11/10/2016.
+ * Created by musz on 10/11/2016.
  */
 
 public class MyAlert {
     //Explicit
     private Context context;
 
-    public MyAlert (Context context){
+    public MyAlert(Context context) {
         this.context = context;
     }
 
-    //Dialog for Error
-    public void myErrorDialog(int intIcon
-                             String strTitle,
-                              String strMessage){
+    // Dialog  for Error
+    public void myErrorDialog(int intIcon,
+                              String strTitle,
+                              String strMessage) {
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setCancelable(false);
         builder.setIcon(intIcon);
@@ -26,10 +27,11 @@ public class MyAlert {
         builder.setMessage(strMessage);
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
             }
         });
         builder.show();
     }
-}
+
+}   // Main Class
